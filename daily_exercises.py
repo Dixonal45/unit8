ad2064cd7ee3a335c80dc0b98d7a8d8d1be4b884
allison <allison.dixon@ssfs.org>
2019-11-18 11:19:17 -0500

    worked on unit 8 exercises

--- daily_exercises.py
+++ daily_exercises.py
@@ -12,9 +12,41 @@ root = Tk()
 # hello_label.grid(row=1, column=1)
 
 # problem 2
-user_name = StringVar()
-enter_name = Entry(root, textvariable=user_name)
-enter_name.grid(row=2, column=1)
-hello_name = Label(root, text="Hello," + user_name)
+
+
+# def say_hi():
+#     other_name = user_name.get()
+#     your_name.set(other_name)
+#     if other_name == "":
+#         your_name.set("You forgot to enter your name")
+#     else:
+#         your_name.set("Hello, " + other_name)
+#
+#
+# user_name = StringVar()
+# your_name = StringVar()
+# enter_name = Entry(root, textvariable=user_name)
+# enter_name.grid(row=1, column=1)
+# say_hello = Button(root, text="Say Hello", command=say_hi)
+# say_hello.grid(row=3, column=1)
+#
+# hello = Label(root, textvariable=your_name)
+# hello.grid(row=2, column=1)
+
+# problem 3
+
+def convert():
+
+degrees_f = Label(root, text="degrees F:")
+degrees_f.grid(row=1, column=1)
+f_entry = Entry(root)
+f_entry.grid(row=1, column=2)
+degrees_c = Label(root, text="degrees C:")
+degrees_c.grid(row=2, column=1)
+c_label = Label(root)
+c_label.grid(row=2, column=2)
+convert = Button(root, text="Convert")
+convert.grid(row=3, column=2)
+
 
 root.mainloop()
